dccdb6402937ab450b6d811871c9faadf68fd2ca
deepsingh-co <deepsinghanirudh1@gmail.com>
2025-06-11 13:21:14 +0530

    Day 1

--- javaBasic.java
+++ javaBasic.java
@@ -1,5 +1,9 @@
 public class javaBasic {
     public static void main(String[] args) {
-        System.out.println("HELLO ANURUDH \n HOW ARE YOU");
+        System.out.println("HELLO ANURUDH \nHOW ARE YOU");
+        System.out.println("* * * *");
+        System.out.println("* * *");
+        System.out.println("* *");
+        System.out.println("*");
     }
 }
